e20136e1e9612eedc3bbc8d543afd2bbbfd9ae6f
NAGARAJAN C <nagarajchinnaiah22@gmail.com>
2025-10-13 14:03:20 +0530

    Add files via upload

---
+++ stirngbuilder.java
@@ -0,0 +1,84 @@
+//  class stringbuilder {
+ 
+//     public static void main(String args[]) {
+//         StringBuilder sb = new StringBuilder("Hello");
+//         System.out.println(sb);
+//     }
+//  }
+//-----------------------------------------------------------------------------------------------------
+// class stringbuilder{
+//     public static void main(String[]args){
+//         sb.append ("Naga");
+//        System.out.println(sb);
+//     }
+// }
+//-----------------------------------------------------------------------------------------------------
+// class stringbuilder{
+//     public static void main(String[]args){
+//         StringBuilder sb=new StringBuilder("Java");
+//         sb.insert(4, " Programming");
+//         System.out.println(sb);
+
+//     }
+// }
+//---------------------------------------------------------------------------------------------------
+// class stringbuilder{
+//     public static void main(String[]args){
+//         StringBuilder sb=new StringBuilder("Hello Java");
+//         System.out.println(sb);
+//         sb.replace(6, 10, "World");
+            //System.out.println(sb);
+
+//     }
+// }
+//---------------------------------------------------------------------------------------------------
+// class stringbuilder{
+//     public static void main(String[]args){
+//         StringBuilder sb=new StringBuilder("Nagarajan");
+//         System.out.println(sb);
+//         sb.delete(3, 6);
+//         System.out.println(sb);
+
+//     }
+// }
+//----------------------------------------------------------------------------------------------------
+// class stringbuilder{
+//     public static void main(String[]args){
+//         StringBuilder sb = new StringBuilder("Naga");
+//         System.out.println(sb);
+//         sb.reverse();
+//         System.out.println(sb);
+//         System.out.println(sb.length());
+
+//     }
+// }
+//------------------------------------------------------------------------------------------------------
+// class stringbuilder{
+//     public static void main(String[]args){
+//         StringBuilder sb = new StringBuilder("Nagarajan");
+//         System.out.println(sb);
+//         sb.setCharAt(0, 'K');
+//         System.out.println(sb);
+//         System.out.println(sb.charAt(1));
+
+//     }
+// }
+//----------------------------------------------------------------------------------------------------
+// class stringbuilder{
+    
+//     public static void main(String[]args){
+//          StringBuilder sb = new StringBuilder("Programming");
+//         System.out.println(sb.substring(0, 6));
+//         System.out.println(sb.substring(3));
+//     }
+// }
+//--------------------------------------------------------------------------------------------------------
+class stringbuilder{
+    public static void main (String[]args){
+         StringBuilder sb=new StringBuilder("Java");
+        String str=sb.toString();
+        System.out.println(sb);
+        System.out.println(str);
+
+    }
+}
