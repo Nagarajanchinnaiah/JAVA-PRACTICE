e3904dda0ee62e62d53d2c81903e530a6ca0c4b4
NAGARAJAN C <nagarajchinnaiah22@gmail.com>
2025-10-13 14:02:57 +0530

    Add files via upload

---
+++ stringbuffer.java
@@ -0,0 +1,46 @@
+// class Stringbuffer { 
+//     public static void main(String[] args) {
+//         StringBuffer sb = new StringBuffer("Hello");
+//         System.out.println(sb);
+//         sb.append(" Java");
+//         System.out.println(sb);
+//     }
+// }
+//----------------------------------------------------------------------------------------------------
+// class Stringbuffer{
+//     puvlic static void main(String[]args){
+//          StringBuffer sb = new StringBuffer("Programming");
+//          sb.insert(11, " in Python");
+//          System.out.println(sb);
+
+//     }
+// }
+//-------------------------------------------------------------------------------------------------------------
+// class stringbuffer{
+//     public static void main(String[]args){
+//          StringBuffer sb = new StringBuffer("Hello World");
+//          sb.replace(6, 11, "Java");
+//          System.out.println(sb);
+
+//     }
+// }
+//------------------------------------------------------------------------------------------------
+// class stringbuffer{
+//     public static void main(string[]args){
+//          StringBuffer sb = new StringBuffer("Computer");
+//          sb.delete(3, 6);
+//         System.out.println(sb);
+
+//     }
+// }
+//-------------------------------------------------------------------------------------------------
+class stringbuffer{
+    public static void main(String[]args){
+        StringBuffer sb = new StringBuffer("Java");
+         sb.reverse();
+         System.out.println(sb);
+         System.out.println(sb.length());
+
+    }
+}
+ 
